06ecb3e53f6a8727cb0c9805b5398eabfcb3187f
Mostafa Sayed <promostafa82@gmail.com>
2022-08-18 20:38:24 +0200

    Add h1 and adjust headings

--- src/components/product/ProductDescription.js
+++ src/components/product/ProductDescription.js
@@ -7,19 +7,19 @@ import classes from './ProductDescription.module.css';
 function ProductDescription(props) {
 	return (
 		<section className={classes['product__description']}>
-			<h2 className={classes['product__company']}>Sneaker Company</h2>
-			<h3 className={classes['product__title']}>
+			<h1 className={classes['product__company']}>Sneaker Company</h1>
+			<h2 className={classes['product__title']}>
 				Fall Limited Edition Sneakers
-			</h3>
+			</h2>
 			<p className={classes['product__desc']}>
 				These low-profile sneakers are your perfect casual wear companion.
 				Featuring a durable rubber outer sole, they’ll withstand everything the
 				weather can offer.
 			</p>
-			<h4 className={classes['product__price']}>
+			<h3 className={classes['product__price']}>
 				$125.00<span className={classes['product__discount']}>50%</span>
-			</h4>
-			<h5 className={classes['product__price--old']}>$250.00</h5>
+			</h3>
+			<h4 className={classes['product__price--old']}>$250.00</h4>
 			<div className={classes['product__order']}>
 				<ProductForm onAddToCart={props.onAddToCart} />
 			</div>
